ad5c4f219d5fa557d9cb17c844bb7eb26dc3e5b6
Anatoliy <toxerius7@gmail.com>
2018-03-14 10:50:52 +0200

    class DataTable

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -28,20 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnExecute = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(55, 94);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(707, 277);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(55, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(462, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(596, 31);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(166, 23);
+            this.btnExecute.TabIndex = 2;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(459, 274);
+            this.ClientSize = new System.Drawing.Size(819, 407);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnExecute;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -7,14 +7,72 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        string connString = ConfigurationManager.ConnectionStrings["MyConn"].ConnectionString;
+        SqlConnection conn = null;
+        SqlDataReader reader;
+        DataTable dataTable;
+
         public Form1()
         {
             InitializeComponent();
+            conn = new SqlConnection(connString);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.CommandText = textBox1.Text;
+                sqlCommand.Connection = conn;
+                dataGridView1.DataSource = null;
+                conn.Open();
+                dataTable = new DataTable();
+                reader = sqlCommand.ExecuteReader();
+
+                int line = 0;
+                do
+                {
+                    while (reader.Read())
+                    {
+                        if(line == 0)
+                        {
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                dataTable.Columns.Add(reader.GetName(i));
+                            }
+                            line++;
+                        }
+                        DataRow dataRow = dataTable.NewRow();
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            dataRow[i] = reader[i];
+                        }
+                        dataTable.Rows.Add(dataRow);
+                    }
+                } while (reader.NextResult());
+                dataGridView1.DataSource = dataTable;
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if(conn != null)
+                {
+                    conn.Close();
+                }
+                if(reader != null){
+                    reader.Close();
+                }
+            }
         }
     }
 }
